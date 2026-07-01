59cb047ee8bb4a6bbe5683262e95fba1bc74efce
Rohitha <rohithamani22@gmail.com>
2026-07-01 16:19:49 +0530

    Upload the LibraryManagement

---
+++ week2/LibraryManagement/src/main/java/com/library/LibraryApplication.java
@@ -0,0 +1,28 @@
+package com.library;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.library.service.BookService;
+import com.library.repository.BookRepository;
+
+public class LibraryApplication {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookService service =
+                context.getBean("bookService", BookService.class);
+
+        BookRepository repository =
+                context.getBean("bookRepository", BookRepository.class);
+
+        service.displayService();
+
+        repository.displayRepository();
+
+    }
+
+}
---
+++ week2/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,7 @@
+package com.library.service;
+
+public class BookService {
+    public void displayService(){
+        System.out.println("BookService Bean Created");
+    }
+}
