7aa492fbba2df2a9ffa76d0379a50e7c18035beb
Rohitha <rohithamani22@gmail.com>
2026-07-03 12:43:00 +0530

    yes

---
+++ week2/orm-learn/src/main/java/com/cognizant/orm_learn/service/CountryService.java
@@ -0,0 +1,28 @@
+package com.cognizant.orm_learn.service;
+
+//package com.cognizant.ormlearn.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
+
+import com.cognizant.orm_learn.model.Country;
+import com.cognizant.orm_learn.repository.CountryRepository;
+
+@Service
+public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @Transactional
+    public List<Country> getAllCountries() {
+
+        return countryRepository.findAll();
+
+    }
+
+}
