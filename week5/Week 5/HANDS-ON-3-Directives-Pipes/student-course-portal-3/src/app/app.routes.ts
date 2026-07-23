b6594001abc705dfb24c18dbcd326be01fad3ff1
rohithamani <rohithamani22@gmail.com>
2026-07-23 20:51:55 +0530

    Delete files

--- week5/Week 5/HANDS-ON-3-Directives-Pipes/student-course-portal-3/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { Home } from './pages/home/home';
-import { CourseList } from './pages/course-list/course-list';
-
-export const routes: Routes = [
-  { path: '', component: Home },
-  { path: 'courses', component: CourseList },
-  { path: '**', redirectTo: '' }
-];
